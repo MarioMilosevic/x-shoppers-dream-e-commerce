f90bcdb79b5a0e790034292e896fb587d438054b
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-05-04 14:53:51 +0200

    Finish hero section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,28 +4,28 @@ const Header = () => {
   return (
     <header className="flex justify-between py-10 pr-1">
       <div className="flex items-center font-medium gap-2 text-lg">
-        <BsBag className="text-xl"/>
-        <span className=" text-slate-700">X Shoppers Dream</span>
+        <BsBag className="text-xl" />
+        <span className=" text-slate-700 font-semibold">X Shoppers Dream</span>
       </div>
       <nav>
-        <ul className="flex gap-8 text-lg font-medium text-slate-700">
-          <li className="hover:underline underline-offset-[6px] decoration-fuchsia-600">
+        <ul className="flex gap-10 text-lg font-medium text-slate-700">
+          <li className="underline underline-offset-[6px] decoration-fuchsia-600">
             <a href="#">Home</a>
           </li>
-          <li className="hover:underline underline-offset-[6px] decoration-fuchsia-600">
+          <li>
             <a href="#">About</a>
           </li>
-          <li className="hover:underline underline-offset-[6px] decoration-fuchsia-600">
+          <li>
             <a href="#">Products</a>
           </li>
         </ul>
       </nav>
       <div className="flex items-center gap-2 text-lg relative">
         <span className="font-medium">Cart</span>
-          <BsBag className="text-xl"/>
-          <span className="bg-fuchsia-600 text-purple-50 text-xs px-1 rounded-full absolute top-2 -right-3">
-            0
-          </span>
+        <BsBag className="text-xl" />
+        <span className="bg-fuchsia-600 text-fuchsia-50 text-xs px-1 rounded-full absolute top-2 -right-2">
+          0
+        </span>
       </div>
     </header>
   );
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,10 +1,31 @@
-
+import heroImg from "../assets/heroImg.a885da91476b940bf532.png";
+import { IoIosArrowRoundDown } from "react-icons/io";
 const HeroSection = () => {
   return (
-    <section>
-      
-    </section>
-  )
-}
+    <section className="flex justify-between py-12">
+      <div className="py-16 w-[650px] flex flex-col gap-7">
+        <h1 className="text-[52px] font-bold leading-snug">
+          Furniture design to <br /> make you feel at home
+        </h1>
+        <h2 className="text-[26px] font-semibold">
+          Create your perfect space with our designs.
+        </h2>
+        <div className="flex gap-4">
+          <button className="bg-fuchsia-600 text-fuchsia-50 font-bold rounded-md px-7 py-3 transition-all duration-200  hover:bg-fuchsia-700">
+            Explore products
+          </button>
+          <button className="font-bold flex items-center gap-1 px-7 py-3 rounded-md transition-all duration-300">
+            <span>Learn more</span>
+            <IoIosArrowRoundDown />
+          </button>
+        </div>
+      </div>
 
-export default HeroSection
+      <div className="w-[515px]">
+        <img src={heroImg} alt="Hero img" />
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
