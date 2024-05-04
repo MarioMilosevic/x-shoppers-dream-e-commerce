29a6f88c881fc2c55879d87f01ac36ce56727db7
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-05-04 12:54:54 +0200

    Finish Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
 function App() {
   return (
-    <Header/>
+    <>
+    <Header />
+    <main>
+        <HeroSection/>
+    </main>
+    </>
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,27 +2,30 @@ import { BsBag } from "react-icons/bs";
 
 const Header = () => {
   return (
-    <header className="border border-black flex justify-between py-12">
-      <div className="flex items-center font-semibold gap-2 text-xl">
-        <BsBag/>
+    <header className="flex justify-between py-10 pr-1">
+      <div className="flex items-center font-medium gap-2 text-lg">
+        <BsBag className="text-xl"/>
         <span className=" text-slate-700">X Shoppers Dream</span>
       </div>
       <nav>
-        <ul className="flex gap-8 text-xl font-medium text-slate-700">
-          <li>
+        <ul className="flex gap-8 text-lg font-medium text-slate-700">
+          <li className="hover:underline underline-offset-[6px] decoration-fuchsia-600">
             <a href="#">Home</a>
           </li>
-          <li>
+          <li className="hover:underline underline-offset-[6px] decoration-fuchsia-600">
             <a href="#">About</a>
           </li>
-          <li>
+          <li className="hover:underline underline-offset-[6px] decoration-fuchsia-600">
             <a href="#">Products</a>
           </li>
         </ul>
       </nav>
-      <div className="border border-black flex items-center gap-2 text-xl">
-        <span>Cart</span>
-        <BsBag />
+      <div className="flex items-center gap-2 text-lg relative">
+        <span className="font-medium">Cart</span>
+          <BsBag className="text-xl"/>
+          <span className="bg-fuchsia-600 text-purple-50 text-xs px-1 rounded-full absolute top-2 -right-3">
+            0
+          </span>
       </div>
     </header>
   );
